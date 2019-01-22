3cf7b9f06a01b13366971c72f3cae9a5a72c46df
pvsile <pvsile@utu.fi>
2019-01-22 21:27:34 +0200

    add harware side

---
+++ README.md
@@ -0,0 +1,4 @@
+# iot-weatherstation
+IoT workshop where we built a weatherstation and a website to inspect the data
+
+Board used: ESP8266
---
+++ iot-weatherstation.ino
@@ -0,0 +1,69 @@
+// ESP8266 Board with temperature, pressure and humidity sensor. Posts data to server as JSON
+
+#include <Arduino.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+#include <Wire.h>
+#include <Adafruit_BME280.h>
+
+#define I2C_ADDR 0x76
+#define SSID ""  //Wifi name
+#define PASSWD ""       //Wifi password 
+#define IP "http://192.168.11.6:3001/newreading"  //Server IP
+#define NAME "Saa-asema"
+
+ESP8266WiFiMulti WiFiMulti;
+Adafruit_BME280 sensor;
+
+const int capacity = JSON_OBJECT_SIZE(4);
+
+void setup() {
+  Serial.begin(115200);
+
+  Wire.begin();
+
+  if(!sensor.begin(I2C_ADDR)){
+    Serial.println("Error: Sensor not found");
+  }
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(SSID, PASSWD);
+}
+
+void loop() {
+  StaticJsonBuffer<capacity> jb;
+  char output[128];
+  JsonObject& data = jb.createObject();
+
+  data["name"] = NAME;
+  data["temperature"] = sensor.readTemperature();
+  data["pressure"] = sensor.readPressure();
+  data["humidity"] = sensor.readHumidity();
+
+  data.printTo(output);
+  data.prettyPrintTo(Serial);
+  Serial.println();
+
+  if(WiFiMulti.run() == WL_CONNECTED){
+
+    HTTPClient http;
+
+    Serial.println("[HTTP] begin...");
+
+    http.begin(IP);
+    http.addHeader("Content-Type", "application/json");
+
+    int httpcode = http.POST(output);
+    
+    if(httpcode == HTTP_CODE_OK){
+      Serial.println("Working");
+    }else{
+      Serial.println("Something went wrong");
+      Serial.println(httpcode);
+    }
+
+    http.end();
+  }
+
+  delay(10000);
+}
